f673175f0e020170b9a20ca29ba39e1b12c8f22d
JackSys-m <jack1qj1@gmail.com>
2025-01-01 00:05:37 +0300

    add image alien.bmp and file alien.py

---
+++ alien.py
@@ -0,0 +1,27 @@
+import os
+
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Класс, представляющий одного пришельца."""
+    
+    def __init__(self, ai_game):
+        """Инициализирует пришельца и задаёт его начальную позицию."""
+        super().__init__()
+        self.screen = ai_game.screen
+        
+        # проверка существования файла изображения
+        if not os.path.exists ('images/alien.bmp'):
+            raise FileNotFoundError ("Файл 'images/alien.bmp' не найден. Убедитесь, что он находится в папке 'images'.")
+        
+        # загрузка изображения пришельца и назначение атрибута rect
+        self.image = pygame.image.load("images/alien.bmp")
+        self.rect = self.image.get_rect()
+        
+        # каждый новый пришелец появляется в левом верхем углу экрана.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        
+        # сохранение точной горизонтальной позиции пришельца
+        self.x = float(self.rect.x)
