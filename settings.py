bd77b52cbd32416e57c9dfeae77b97155e8fb5ee
JackSys-m <jack1qj1@gmail.com>
2025-01-02 20:32:45 +0300

    create settings for move aliens drop

--- settings.py
+++ settings.py
@@ -18,5 +18,8 @@ class Settings():
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
         
-        #настройка пришельцев
+        # настройки пришельцев
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_diection = 1 обозначает движение вправо; а -1 - влево
+        self.fleet_diection = 1
